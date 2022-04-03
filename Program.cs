61b87af79e6e923a7ed389639e3ab9fba4d9a003
AbhishekDevlop <101594544+AbhishekDevlop@users.noreply.github.com>
2022-04-03 11:07:21 +0530

    Add files via upload
    Abstraction shape example

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle(10,20);         
+            rect.area();
+            Console.WriteLine(rect);
+
+            Circle cir = new Circle(9);
+            cir.area();
+            Console.WriteLine(cir);
+
+        }
+    }
+}
---
+++ Shape.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    abstract public class Shape
+    {
+        abstract public void area();
+    }
+
+    public class Rectangle : Shape
+    {
+        private int length, width;
+        private double result, PI=3.14;
+
+        public Rectangle(int length,int width)
+        {
+            this.length = length;
+            this.width = width;
+
+        }
+
+        public override void area() 
+        {
+            result = length * width;
+        }
+
+        public override string ToString()
+        {
+            return $"Area of Reactangele = {result} sq.";
+        }
+
+    }
+
+    class Circle : Shape
+    {
+        int radius;
+        double result, PI = 3.14;
+
+        public Circle(int radius)
+        {
+            this.radius = radius;
+        }
+        public override void area()
+        {
+            result = PI*radius*radius;
+
+        }
+        public override string ToString()
+        {
+            return $"Area of Circle = {result} sq."; 
+        }
+    }
+}
